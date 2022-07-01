3188dc1c047e80ef9fc80a388a94c5d1dab50e95
unknown <sharique061@gmail.com>
2022-07-01 12:59:05 +0500

    fg

--- components/Footer/Social.js
+++ components/Footer/Social.js
@@ -12,42 +12,42 @@ const Social = () => {
   return (
     <div className={style.wrapper}>
       <a
-        aria-label="link to facebook account facebook"
+        aria-label="link to  account facebook"
         className={style.social}
         target="_black"
-        href="http://www.goolge.com"
+        href="http://www.facebook.com"
       >
         <BsFacebook className="text-2xl" />
       </a>
       <a
-        aria-label="link to facebook account instagram"
+        aria-label="link to  account instagram"
         className={style.social}
         target="_black"
-        href="http://www.goolge.com"
+        href="http://www.instagram.com"
       >
         <BsInstagram className="text-2xl" />
       </a>
       <a
-        aria-label="link to facebook account twitter"
+        aria-label="link to  account twitter"
         className={style.social}
         target="_black"
-        href="http://www.goolge.com"
+        href="http://www.twitter.com"
       >
         <BsTwitter className="text-2xl" />
       </a>
       <a
-        aria-label="link to facebook account twitter"
+        aria-label="link to  account twitter"
         className={style.social}
         target="_black"
-        href="http://www.goolge.com"
+        href="http://www.twitter.com"
       >
         <AiFillYoutube className="text-2xl" />
       </a>
       <a
-        aria-label="link to facebook account twitter"
+        aria-label="link to  account discord"
         className={style.social}
         target="_black"
-        href="http://www.goolge.com"
+        href="http://www.discord.com"
       >
         <FaDiscord className="text-2xl" />
       </a>
