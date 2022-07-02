ed738157b92b849b8b9f897ee7afbb963a290544
unknown <sharique061@gmail.com>
2022-07-02 12:40:50 +0500

    dsf

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,21 @@
 import "../styles/globals.css";
 import Head from "next/head";
 
-import Footer from '../components/Footer/Footer'
+import Footer from "../components/Footer/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-      <title key={'title'}>Power Rangers Free</title>
+        <title key={"title"}>Power Rangers Free</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Online power ranger games"
-          />
+        <meta name="description" content="Online power ranger games" />
       </Head>
-      <div className="overflow-x-hidden main-bg" style={{ fontFamily: "Poppins, sans-serif" }}
->
-            <Component {...pageProps} />
+      <div
+        className="overflow-x-hidden main-bg"
+        style={{ fontFamily: "Poppins, sans-serif", scrollBehaviour: "smooth" }}
+      >
+        <Component {...pageProps} />
         <Footer />
       </div>
     </>
